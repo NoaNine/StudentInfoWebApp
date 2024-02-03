99fc429f5fa86d1c297c899861a3814472c6df24
Oleksandr Havlytskyi <havlytskyi@gmail.com>
2024-02-03 17:23:54 +0200

    Added view groups

--- StudentInfoWebApp.Web/Controllers/GroupController.cs
+++ StudentInfoWebApp.Web/Controllers/GroupController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StudentInfoWebApp.Core.Services;
+using StudentInfoWebApp.Core.Services.Interface;
 using StudentInfoWebApp.Web.Models;
 using System.Diagnostics;
 
@@ -6,9 +8,24 @@ namespace StudentInfoWebApp.Web.Controllers;
 
 public class GroupController : Controller
 {
-    public IActionResult Index()
+    private readonly IGroupService _groupService;
+
+    public GroupController(IGroupService groupService)
     {
-        return View();
+        _groupService = groupService ?? throw new ArgumentNullException(nameof(groupService));
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        try
+        {
+            var groups = await _groupService.GetAllGroups();
+            return View(groups);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
